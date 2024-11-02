b7dacd9ae32634e57e921c5d84dd2675d1826278
Nicolas Trossat <nicolas.trossat@gmail.com>
2024-11-02 14:52:39 +0100

    Clean DB config

--- core/postgres/db.py
+++ core/postgres/db.py
@@ -5,14 +5,8 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-# TODO Turn echo to False
 DATABASE_URL = os.environ.get("DATABASE_URL")
-engine = create_async_engine(DATABASE_URL, echo=True, future=True)
-
-
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
+engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 
 
 async def get_session() -> AsyncSession:
