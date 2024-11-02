b58b2b6184623ad98f8098cf36dab40622af24d3
Nicolas Trossat <nicolas.trossat@gmail.com>
2024-11-02 14:52:10 +0100

    Configure Alembic

---
+++ api/models/__init__.py
@@ -0,0 +1,4 @@
+from sqlmodel import SQLModel
+
+from api.models.activity import Activity
+
--- core/alembic/env.py
+++ core/alembic/env.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 from logging.config import fileConfig
 
@@ -20,12 +21,18 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+from api.models import SQLModel
+
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 
 def run_migrations_offline() -> None:
