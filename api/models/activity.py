69e2ab6d21e9793cc4a39b37e6d977d49a3244a0
Nicolas Trossat <nicolas.trossat@gmail.com>
2024-11-02 14:54:55 +0100

    Refactor Activity

--- api/models/activity.py
+++ api/models/activity.py
@@ -4,13 +4,10 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 
 
-class ActivityBase(SQLModel):
+class Activity(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     name: str
     description: str
     type: Optional[str] = None
     start_date: datetime
     end_date: datetime
-
-
-class Activity(ActivityBase, table=True):
-    id: int = Field(nullable=False, primary_key=True)
